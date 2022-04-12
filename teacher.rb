4571bb202247fa890311788f62373fc38e9e1d5c
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-04-12 15:25:33 +0300

    Update file to refactor code

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,9 @@
-require_relative './person'
+require './person'
 
 class Teacher < Person
-  def initialize(age, name, specializaton)
-    super(age, name)
-    @specializaton = specializaton
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @specialization = specialization
   end
 
   def can_use_services?
