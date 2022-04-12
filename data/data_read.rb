393e7f27ce75cf62a8fc590f74be3228953913cb
Natalia Poletaeva <natfly799@gmail.com>
2022-04-12 17:56:48 +0500

    Add object for creating storage for book and rentals

--- data/data_read.rb
+++ data/data_read.rb
@@ -13,3 +13,25 @@ def read_person
     []
   end
 end
+
+def read_book
+  if File.exist?('./data/books.json')
+    books = JSON.parse(File.read('./data/books.json'))
+    books.map do |book|
+      Book.new(book['title'], book['author'])
+    end
+  else
+    []
+  end
+end
+
+def read_rentals
+  if File.exist?('./data/rentals.json')
+    rent = JSON.parse(File.read('./data/rentals.json'))
+    rent.map do |rental|
+      Rental.new(rental['date'], @persons[rental['person_index']], @books[rental['book_index']])
+    end
+  else
+    []
+  end
+end
