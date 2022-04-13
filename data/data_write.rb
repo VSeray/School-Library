0e60ae504bcf8fcf1d5a3a985876f55c6054390e
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-13 08:34:41 +0100

    Add methods to save books & rentals

--- data/data_write.rb
+++ data/data_write.rb
@@ -20,3 +20,37 @@ def save_persons
     file.puts(JSON.pretty_generate(person_store))
   end
 end
+
+def save_books
+  file = File.open('./data/books.json', 'w')
+  book_store = @books.map do |book|
+    { title: book.title, author: book.author }
+  end
+  file.puts(JSON.pretty_generate(book_store))
+end
+
+def save_rentals(date, person_id, book_id)
+  new_data = []
+  if File.exist?('./data/rentals.json')
+    rents = JSON.parse(File.read('./data/rentals.json'))
+    rents.map do |ren|
+      new_data.push(ren)
+    end
+  else
+    []
+  end
+  new_data.push({ Date: date, book_index: book_id, person_index: person_id })
+  File.open('./data/rentals.json', 'w') do |file|
+    file.puts(JSON.pretty_generate(new_data))
+  end
+end
+
+
+
+
+
+
+
+
+
+
